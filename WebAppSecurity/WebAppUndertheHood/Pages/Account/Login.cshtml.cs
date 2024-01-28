6acc792c00d24916bd980f85119cdb37d42ca591
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-01-28 20:30:21 +0200

    Cookie Lifetime, Browser Session and Persistent

--- WebAppSecurity/WebAppUndertheHood/Pages/Account/Login.cshtml.cs
+++ WebAppSecurity/WebAppUndertheHood/Pages/Account/Login.cshtml.cs
@@ -34,8 +34,13 @@ namespace WebAppUndertheHood.Pages.Account
                 var identity = new ClaimsIdentity(claims,"MyCookieAuth");
                 ClaimsPrincipal principal = new ClaimsPrincipal(identity);
 
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = Credential.RememberMe
+                };
+
                 // Serialize the claims principal into string then encrypt that string and save that as a cookie
-                await HttpContext.SignInAsync("MyCookieAuth", principal); // Request Encapsulated into HttpContext
+                await HttpContext.SignInAsync("MyCookieAuth", principal, authProperties); // Request Encapsulated into HttpContext
                 return RedirectToPage("/Index");
             }
             return Page();
@@ -50,5 +55,7 @@ namespace WebAppUndertheHood.Pages.Account
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }    
     }
 }
--- WebAppSecurity/WebAppUndertheHood/Startup.cs
+++ WebAppSecurity/WebAppUndertheHood/Startup.cs
@@ -30,6 +30,7 @@ namespace WebApp_UnderTheHood
                 options.Cookie.Name = "MyCookieAuth";
                 options.LoginPath = "/Account/Login";
                 options.AccessDeniedPath = "/Account/AccessDenied";
+                options.ExpireTimeSpan = TimeSpan.FromSeconds(20);
             });
 
             services.AddAuthorization(options =>

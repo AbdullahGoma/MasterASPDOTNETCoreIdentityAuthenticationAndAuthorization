fb7bacdd1cd2b0ae5bc315b6df58239936fce1a2
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-01-29 19:58:14 +0200

    Create Client from Http Client Factory to deal with WeatherForecast and Add WeatherForecastDTO to recieve WeatherForecast in it

--- WebAppSecurity/WebAppUndertheHood/Pages/HRManager.cshtml.cs
+++ WebAppSecurity/WebAppUndertheHood/Pages/HRManager.cshtml.cs
@@ -1,14 +1,29 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppUndertheHood.DTO;
 
 namespace WebAppUndertheHood.Pages
 {
     [Authorize(Policy = "HRManagerOnly")]
     public class HRManagerModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory httpClientFactory;
+
+        [BindProperty]
+        public List<WeatherForecastDTO> whateverForecastItems { get; set; } = new List<WeatherForecastDTO>();
+
+        public HRManagerModel(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task OnGetAsync()
+        {
+            var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+            whateverForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast") ?? 
+                                                                                                new List<WeatherForecastDTO>();          
         }
     }
 }

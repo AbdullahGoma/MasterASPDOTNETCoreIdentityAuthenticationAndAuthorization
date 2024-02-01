51a9abfb31bb63fa0e8396d1b81e9f5d4b89f8a5
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-02-01 22:16:09 +0200

    Add Sign Out

--- WebAppSecurity/WebApp/Services/EmailService.cs
+++ WebAppSecurity/WebApp/Services/EmailService.cs
@@ -17,25 +17,25 @@ namespace WebApp.Services
         }
         public async Task SendAsync(string from, string to, string subject, string body)
         {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Abdullah Gomaa", from));
-            message.To.Add(new MailboxAddress("Recipient Name", to));
-            message.Subject = subject;
-            message.Body = new TextPart("plain") { Text = body };
-            //var message = new MailMessage(from, to, subject, body);
-            //using (var emailClient = new SmtpClient(smtpSetting.Value.Host, smtpSetting.Value.Port))
-            //{
-            //    emailClient.Credentials = new NetworkCredential(smtpSetting.Value.User, smtpSetting.Value.Password);
-            //    emailClient.EnableSsl = true;
-            //    await emailClient.SendMailAsync(message);
-            //}
-            using (var emailClient = new MailKit.Net.Smtp.SmtpClient())
+            //var message = new MimeMessage();
+            //message.From.Add(new MailboxAddress("Abdullah Gomaa", from));
+            //message.To.Add(new MailboxAddress("Recipient Name", to));
+            //message.Subject = subject;
+            //message.Body = new TextPart("plain") { Text = body };
+            var message = new MailMessage(from, to, subject, body);
+            using (var emailClient = new SmtpClient(smtpSetting.Value.Host, smtpSetting.Value.Port))
             {
-                await emailClient.ConnectAsync(smtpSetting.Value.Host, smtpSetting.Value.Port, SecureSocketOptions.StartTls);
-                await emailClient.AuthenticateAsync(smtpSetting.Value.User, smtpSetting.Value.Password);
-                await emailClient.SendAsync(message);
-                await emailClient.DisconnectAsync(true);
+                emailClient.Credentials = new NetworkCredential(smtpSetting.Value.User, smtpSetting.Value.Password);
+                emailClient.EnableSsl = true;
+                await emailClient.SendMailAsync(message);
             }
+            //using (var emailClient = new MailKit.Net.Smtp.SmtpClient())
+            //{
+            //    await emailClient.ConnectAsync(smtpSetting.Value.Host, smtpSetting.Value.Port, SecureSocketOptions.StartTls);
+            //    await emailClient.AuthenticateAsync(smtpSetting.Value.User, smtpSetting.Value.Password);
+            //    await emailClient.SendAsync(message);
+            //    await emailClient.DisconnectAsync(true);
+            //}
         }
     }
 }
